f0248e521826ea90d3aadfc3833af0ffb803aade
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-22 16:03:55 +0100

    setup

---
+++ src/main/java/com/taha/securebankingapp/SecureBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.taha.securebankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBankingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taha/securebankingapp/SecureBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taha.securebankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
